1e24ff2486990fc8f409c42e6628c68d28eef9d8
luismagnum <luismagnum1@gmail.com>
2024-05-27 15:31:00 -0300

    crear ection

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,13 @@
 
 const Home = () => {
   return (
-
+    <section id="Seccion1">
     <div className='min-h-80 md:min-h-screen h-auto flex flex-col justify-center items-center lg:px-32 px-5 bg-[url(https://images.unsplash.com/photo-1539185441755-769473a23570?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTcwfHx6YXBhdGlsbGElMjBkZSUyMGRlcG9ydGV8ZW58MHx8MHx8fDA%3D)] bg-contain md:bg-cover bg-center bg-no-repeat w-full'>
       <div className='w-full lg:w-2/3 space-y-5'>
         <h1 className='text-black font-bold text-xl md:text-4xl'></h1>
       </div>
     </div>
+    </section>
   )
 }
 
